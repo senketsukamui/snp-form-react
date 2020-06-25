f1c8fcb5e7c96c5afbc33e14ed90d9e5065fd167
Gleb Potapov <glebpotapov2001@gmail.com>
2020-06-25 23:47:05 +0300

    Create basic design

--- src/components/Document/index.jsx
+++ src/components/Document/index.jsx
@@ -5,6 +5,17 @@ const Document = () => {
   return (
     <div className={styles.document}>
       <h1 className={styles.title}>Стажировка в Salt & Pepper</h1>
+      <div className={styles.form_field}>ФИО стажёра:</div>
+      <div className={styles.form_field}>Дата рождения:</div>
+      <div className={styles.form_field}>Образование:</div>
+      <div className={styles.form_field}>Место работы:</div>
+      <div className={styles.form_field}>Опыт в разработке:</div>
+      <div className={styles.form_field}>Технологии/умения:</div>
+      <div className={styles.form_field}>О себе:</div>
+      <div className={styles.form_field}>Уровень владения английским:</div>
+      <div className={styles.form_field}>Опыт в разработке:</div>
+      <div className={styles.form_field}>Что хочешь изучать:</div>
+      <div className={styles.form_field}>Контакты:</div>
     </div>
   );
 };
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -2,7 +2,39 @@ import React from "react";
 import styles from "./index.module.css";
 
 const Form = () => {
-  return <div className={styles.form}>Form</div>;
+  return (
+    <form className={styles.form} action="novalidate">
+      <div className={styles.title}>Форма для стажировки</div>
+      <p className={styles.description}>
+        Пожалуйста, заполни как можно подробнее данную заявку. Если у тебя есть
+        готовое резюме, укажи на него ссылку.
+      </p>
+      <p className={styles.input_title}>ФИО</p>
+      <input type="text" className={styles.input} />
+      <p className={styles.input_title}>Дата рождения</p>
+      <input type="text" className={styles.input} />
+      <p className={styles.input_title}>Образование</p>
+      <input type="text" className={styles.input} />
+      <p className={styles.input_title}>Место работы</p>
+      <input type="text" className={styles.input} />
+      <p className={styles.input_title}>Опыт в разработке</p>
+      <input type="text" className={styles.input} />
+      <p className={styles.input_title}>Технологии/умения</p>
+      <input type="text" className={styles.input} />
+      <p className={styles.input_title}>О себе</p>
+      <input type="text" className={styles.input} />
+      <p className={styles.input_title}>Уровень владения английским</p>
+      <input type="text" className={styles.input} />
+      <p className={styles.input_title}>Опыт в разработке</p>
+      <input type="text" className={styles.input} />
+      <p className={styles.input_title}>Что хочешь изучать</p>
+      <input type="text" className={styles.input} />
+      <p className={styles.input_title}>Контакты</p>
+      <input type="text" className={styles.input} />
+      <p className={styles.input_title}>Введите ФИО</p>
+      <input type="text" className={styles.input} />
+    </form>
+  );
 };
 
 export default Form;
