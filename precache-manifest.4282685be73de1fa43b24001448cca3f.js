f651097bd5765d986f6461b0d07b12fe7f422735
Gleb Potapov <glebpotapov2001@gmail.com>
2020-06-30 17:26:34 +0300

    Updates

---
+++ precache-manifest.4282685be73de1fa43b24001448cca3f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f788253738f7269d52bf588ef71ff0",
+    "url": "/snp-form-react/index.html"
+  },
+  {
+    "revision": "a92e16ad09bef7552b78",
+    "url": "/snp-form-react/static/css/main.bf8bc455.chunk.css"
+  },
+  {
+    "revision": "72fef53db0a87e6c7bd6",
+    "url": "/snp-form-react/static/js/2.13ab760e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/snp-form-react/static/js/2.13ab760e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a92e16ad09bef7552b78",
+    "url": "/snp-form-react/static/js/main.d5c17c6f.chunk.js"
+  },
+  {
+    "revision": "369b9e0c70ca3c5bc1ed",
+    "url": "/snp-form-react/static/js/runtime-main.7ee44219.js"
+  },
+  {
+    "revision": "a3d7d652af07c3610c453b4a51c788c7",
+    "url": "/snp-form-react/static/media/GothamPro-Bold.a3d7d652.woff"
+  },
+  {
+    "revision": "b86e995a10856a8b8e222fe8ff00a74f",
+    "url": "/snp-form-react/static/media/GothamPro-Medium.b86e995a.woff"
+  },
+  {
+    "revision": "3cb46b372ab77d032576b9f70c83d1ff",
+    "url": "/snp-form-react/static/media/GothamPro.3cb46b37.woff"
+  },
+  {
+    "revision": "353166d27ed040ea43a7fcb538da7c3c",
+    "url": "/snp-form-react/static/media/Vinnytsia.353166d2.woff"
+  }
+]);
